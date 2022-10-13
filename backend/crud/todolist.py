8faf3e90226374b4ab3a90807f355bd1abb4c608
Nithinbarath <nithinvp3599@gmail.com>
2022-10-13 18:39:01 +0530

    get list

--- backend/crud/todolist.py
+++ backend/crud/todolist.py
@@ -1,27 +1,26 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
-from schemas import question as schema_question
-from models import question as model_question
-from models import question
-from schemas.todolist import CreateTodolist
+
 from models.todolist import Todolist
-
-def get_by_question(db: Session,question: str):
-
-    return db.query(model_question.Question).filter(model_question.Question.question== question).first()
-
-def create_question(db: Session, questions: schema_question.CreateQuestions):
-    db_question = model_question.Question(question=questions.question,optionA = questions.optionA,optionB = questions.optionB,optionC = questions.optionC,optionD = questions.optionD)
-    db.add(db_question)
-    db.commit()
-    db.refresh(db_question)
-    return db_question
+from schemas.todolist import CreateTodolist
 
 
 
-def create_list(session: Session, note:CreateTodolist):
-    
-    db_todolist = Todolist(notes=note.notes)
-    session.add(db_todolist)
-    session.commit()
-    session.refresh(db_todolist)
-    return db_todolist
+def create_list(session: Session, list:CreateTodolist):
+
+    record = session.query(Todolist).filter(Todolist.notes == list.notes).first()
+
+    if record:
+        raise HTTPException(status_code=400, detail="Notes Already Created")
+    else:
+        db_todolist = Todolist(notes=list.notes)
+        session.add(db_todolist)
+        session.commit()
+        session.refresh(db_todolist)
+        return db_todolist
+
+def get_todolist(session: Session):
+
+    record = session.query(Todolist).all()
+
+    return record
--- backend/endpoint/todolist.py
+++ backend/endpoint/todolist.py
@@ -2,14 +2,18 @@ from fastapi import APIRouter,Depends,HTTPException,status
 from sqlalchemy.orm import Session
 
 from schemas.todolist import CreateTodolist
-from crud.todolist import create_list
+from crud.todolist import create_list, get_todolist
 from application import get_db_session
 
 router = APIRouter()
 
 
-@router.post("/todolist",tags=["todolist"])
+@router.post("/create/todolist",tags=["todolist"])
 def create_todolist(list:CreateTodolist, sessipn: Session = Depends(get_db_session)):
 
-    return create_list(session=sessipn,note=list)
-    
+    return create_list(session=sessipn,list=list)
+
+@router.get("/list/todolist" ,tags=['todolist'])
+def list_todolist(session: Session = Depends(get_db_session)):
+
+    return get_todolist(session=session)
